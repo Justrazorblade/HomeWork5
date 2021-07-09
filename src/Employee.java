21c13a7321926fd694eaa99ff3dcfcb18690f369
Justrazorblade <dark_warrior-07@mail.ru>
2021-07-09 23:09:17 +0300

    HomeWork5

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,28 @@
+public class Employee {
+    String fName;;
+    String lName;
+    String position;
+    String pNumber;
+    String email;
+    float salary;
+    int age;
 
+    void personInfo (){
+        System.out.println(fName +' ' + lName);
+        System.out.println("Phone number: " + pNumber);
+        System.out.println("Email: " + email);
+        System.out.println("Position: " + position);
+        System.out.println("Salary: " + salary);
+        System.out.println("Age: " + age);
+    }
+
+    public Employee(String fName, String lName, String position, String pNumber, String email, float salary, int age){
+        this.fName = fName;
+        this.lName = lName;
+        this.position = position;
+        this.pNumber = pNumber;
+        this.email = email;
+        this.salary = salary;
+        this.age = age;
+    }
+}
